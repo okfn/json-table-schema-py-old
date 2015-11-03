fa011776bac12c8f113f177ed555151b915e9b13
Brook Elgie <specialbrew@gmail.com>
2015-11-03 14:17:44 +0000

    Deprecate json-table-schema.
    See instead https://github.com/okfn/jsontableschema-py

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-JSON Table Schema
-=================
+JSON Table Schema - deprecated
+==============================
 
-Deprecated. Use https://github.com/okfn/json-table-schema-py
+This package is no longer supported and has been deprecated in favour of **jsontableschema**: https://github.com/okfn/jsontableschema-py
+
+Please install jsontableschema:
+
+```pip install jsontableschema```
+
+As of json-table-schema==0.5.0, installing this package will install jsontableschema as a dependency. 
--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
-#coding: utf-8
 
 from setuptools import setup
 
+import io
+import os
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+README = 'README.md'
+README_PATH = os.path.join(DIR, README)
+
+print("json-table-schema has been replaced by jsontableschema. "
+      "See https://github.com/okfn/json-table-schema-py-old for details.")
+
+
+with io.open(README_PATH, mode='r+t', encoding='utf-8') as stream:
+    description_text = stream.read()
+
 setup(
-	name = "json-table-schema",
-	author = "Martin Keegan",
-	author_email = "martin.keegan@okfn.org",
-	version = "0.2",
-	license = "Apache Licence v2.0",
-	url = "",
-	download_url = "",
-	description = "Python library for dealing with JSON Table Schema files",
-	py_modules = "",
-	packages = ["jsontableschema"],
-	install_requires = "",
-	scripts = ""
+    name="json-table-schema",
+    author="Open Knowledge",
+    author_email="info@okfn.org",
+    url="https://github.com/okfn/json-table-schema-py-old",
+    version="0.5.0",
+    description="Python library for dealing with JSON Table Schema files",
+    long_description=description_text,
+    install_requires=['jsontableschema>=0.5.1'],
+    classifiers=['Development Status :: 7 - Inactive']
 )
